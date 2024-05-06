67cb0d7cabf123e8f9dc4c4aca84d7ed5a9a1851
seth-kerner <118932240+seth-kerner@users.noreply.github.com>
2024-05-06 10:10:27 -0400

    Update streamlit_app.py
    Output fruityvice info as json

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,4 +37,4 @@ if ingredients_list:
 
 # New section to get fruityvice nutrition information
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
